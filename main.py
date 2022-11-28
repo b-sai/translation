34b43e1486e15d4bae1334b5397beb7da4881177
b-sai <bsaishreyas@gmail.com>
2022-11-27 18:06:53 -0600

    using spelling enhanced model

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from utils import *
+import pickle
 import streamlit as st
 from gensim.models import Word2Vec, KeyedVectors
 from tensorflow import keras
-st.title('English - French Translation Tool')
+
+st.markdown('# English to French Translation Tool')
+st.markdown('<p style="color:grey; font-size: 18px;">By Sai Shreyas Bhavanasi</p>',
+            unsafe_allow_html=True)
+
 if 'count' not in st.session_state:
     st.session_state['count'] = ""
 if 'key' not in st.session_state:
     st.session_state.key = 'value'
+if 'clicks' not in st.session_state:
+    st.session_state['clicks'] = 0
 
 
 @st.cache(allow_output_mutation=True)
@@ -20,7 +28,7 @@ def get_xmod():
 def get_ymod():
     y_mod = KeyedVectors.load_word2vec_format(
         'fr_20k.vecs', binary=False)
-    print()
+    print('here')
 
     return y_mod
 
@@ -33,6 +41,16 @@ def get_model():
     return model
 
 
+@st.cache(allow_output_mutation=True)
+def get_tables():
+    with open('tables.pkl', 'rb') as f:
+        tables = pickle.load(f)
+    ins_table = tables['ins']
+    del_table = tables['del']
+    sub_table = tables['sub']
+    return ins_table, del_table, sub_table
+
+
 def get_nn_preds(y_mod, mod_res):
     predictions = y_mod.most_similar(mod_res, topn=10)
     assert len(predictions) == 10
@@ -43,9 +61,14 @@ def get_nn_preds(y_mod, mod_res):
 x_mod = get_xmod()
 model = get_model()
 y_mod = get_ymod()
+if 'ins_table' not in st.session_state:
+    st.session_state['ins_table'], st.session_state['del_table'], st.session_state['sub_table'] = get_tables()
 
 
 def get_translation():
+
+    st.session_state.clicks += 1
+
     input_word = st.session_state['inps']
     input_word = input_word.lower()
     try:
@@ -55,31 +78,32 @@ def get_translation():
             " not in model vocabulary"
         return
     out_vec = model.predict(input_vec)
-    preds = get_nn_preds(y_mod, out_vec)
-    # print(out_vec)
+    preds = get_enhanced_preds(
+        y_mod, out_vec, input_word,  st.session_state['ins_table'], st.session_state['del_table'], st.session_state['sub_table'])
     st.session_state.count = preds
-
-
-def demo(inp):
-    st.write(st.session_state.inps + inp)
 
 
 st.text_input('Enter English Word', on_change=get_translation,
               key='inps', value="dog")
 st.button('Get Translation', on_click=get_translation)
 
-st.write("Top 10 French Translations:")
-print(type(st.session_state.count), st.session_state.count)
-if type(st.session_state.count) is tuple:
-    s = ''
-    for pred in st.session_state.count:
-        s += "- " + pred + "\n"
-    st.markdown(s)
-else:
-    st.write(st.session_state.inps + " not in vocabulary")
+
+if st.session_state.clicks > 0:
+    st.write("Top 10 French Translations:")
+    if type(st.session_state.count) is list:
+        s = ''
+        for pred in st.session_state.count:
+            s += "- " + pred + "\n"
+        st.markdown(s)
+    else:
+        st.write(st.session_state.inps + " not in vocabulary")
 
 st.write("""
+         ---
          The translation model is a replication of the paper [Exploiting Similarities among Languages for Machine Translation](https://arxiv.org/pdf/1309.4168.pdf). 
-         To generate the translations, I use word embeddings from 
-         [fasttext by Meta AI Research](https://fasttext.cc/docs/en/crawl-vectors.html) and find a linear mapping between English and French word embeddings using keras.
+         This model is unsupervised i.e. translates with minimal parallel data (5000 examples). Traditonal supervised translations require large 
+         corpuses of parallel translated data which doesn't scale well. This model has applications such as language translation and extending dictionaries.
+         
+         Further, some french words have similar translations to their english counterparts (cat -> chat/chatte). To exploit this property, I refine the predictions
+         by ranking the top 400 predictions based on the edit distance (spelling similarity).
          """)
---
+++ utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+import math
+from copy import deepcopy
+import torch
+
+
+def get_score(a, b, freq_table, substition=False):
+    if a == b and substition:
+        return 0.000000001
+    if(a, b) in freq_table:
+        return freq_table[(a, b)]
+    else:
+        return 9e2
+
+
+def enhanced_levenshtein(s1, s2, ins_table, del_table, sub_table):
+
+    s1 = "#"+s1
+    s2 = "#"+s2
+
+    rows = []
+
+    previous_row = [0]
+    for i in range(1, len(s2)):
+        previous_row.append(
+            previous_row[i-1] + get_score(s2[i-1], s2[1], freq_table=ins_table))
+    rows.append(previous_row)
+    for i in range(1, len(s1)):
+        current_row = [
+            get_score(s1[i], s1[i-1], freq_table=del_table)+previous_row[0]]
+        for j in range(1, len(s2)):
+            ins_score = get_score(
+                s2[j], s1[i], freq_table=ins_table) + current_row[j-1]
+            del_score = get_score(
+                s1[i], s1[i-1], freq_table=del_table)+previous_row[j]
+            sub_score = get_score(
+                s1[i], s2[j], freq_table=sub_table, substition=True)+previous_row[j-1]
+            current_row.append(min(ins_score, del_score, sub_score))
+        rows.append(current_row)
+        previous_row = current_row
+    return previous_row[-1]/max(len(s1), len(s2))
+
+
+def get_enhanced_preds(y_mod, pred_vec: np.array, src_word: str,  ins_table, del_table, sub_table):
+    pred_words = y_mod.most_similar(pred_vec, topn=400)
+    words = [item[0] for item in pred_words]
+    avg_scores = []
+    for word, cos_dist in pred_words:
+        score = enhanced_levenshtein(
+            src_word, word, ins_table, del_table, sub_table)
+        avg_scores.append((1-cos_dist)+(score/40))
+    avg_idxs = torch.topk(torch.tensor(avg_scores), 10,
+                          sorted=True, largest=False)[1]
+    avg_preds = [words[i] for i in avg_idxs]
+    return avg_preds
